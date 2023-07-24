ccde6a452de4337c1b2931d575a1c4eb55e95e89
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-24 06:07:02 +0300

    Proje dosyası ekle.

---
+++ Donguler5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen sayinin faktoriyel degerini hesaplayan kod blogu
+
+            int sayi;
+            Console.WriteLine("Lutfen faktoriyelini hesaplamak istediginiz bir sayi giriniz :");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            int i;
+            int faktoriyel = 1;
+
+            for (i = 1; i <= sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+            }
+
+            Console.WriteLine(faktoriyel);
+
+
+            Console.Read();
+        }
+    }
+}
